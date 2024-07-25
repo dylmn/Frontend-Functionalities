74ed939e99c92c6e8147b37a16a03697f75e8025
dylmn <67588622+dylmn@users.noreply.github.com>
2024-07-25 21:23:45 +0800

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,11 @@ const hre = require("hardhat");
 
 async function main() {
   const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy(initBalance);
-  await assessment.deployed();
+  const petCurrency = await hre.ethers.getContractFactory("petCurrency");
+  const petcurrency = await petCurrency.deploy(initBalance);
+  await petcurrency.deployed();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${petcurrency.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
